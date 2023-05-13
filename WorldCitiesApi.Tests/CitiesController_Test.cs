5ae677faa3d9364d3f1f581f5598ed5d14642a2c
Mustafa Aleem <maleem@intralinks.com>
2023-05-13 19:37:54 +0100

    Chapter 10 complete

---
+++ WorldCitiesApi.Tests/CitiesController_Test.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using WorldCitiesApi.Controllers;
+using WorldCitiesApi.Data;
+using WorldCitiesApi.Data.Models;
+
+namespace WorldCitiesApi.Tests
+{
+    public class CitiesController_Test
+    {
+        /// <summary>
+        /// Test the GetCity() method
+        /// </summary>
+        [Fact]
+        public async Task GetCity()
+        {
+            // Arrange
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "WorldCities")
+                .Options;
+            using var context = new ApplicationDbContext(options);
+            context.Add(new City()
+            {
+                Id = 1,
+                CountryId = 1,
+                Lat = 1,
+                Lon = 1,
+                Name = "TestCity1"
+            });
+            context.SaveChanges();
+            var controller = new CitiesController(context);
+            City? city_existing = null;
+            City? city_notExisting = null;
+            // Act
+            city_existing = (await controller.GetCity(1)).Value;
+            city_notExisting = (await controller.GetCity(2)).Value;
+            // Assert
+            Assert.NotNull(city_existing);
+            Assert.Null(city_notExisting);
+        }
+    }
+}
