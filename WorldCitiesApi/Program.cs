a1868c6994fe912e90bbcaa905a838fe263779f6
Mustafa Aleem <maleem@intralinks.com>
2023-05-13 19:12:27 +0100

    Chapter 9 complete

--- WorldCitiesApi/Program.cs
+++ WorldCitiesApi/Program.cs
@@ -1,4 +1,9 @@
 using Microsoft.EntityFrameworkCore;
+using NpgsqlTypes;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.PostgreSQL;
+using Serilog.Sinks.PostgreSQL.ColumnWriters;
 using WorldCitiesApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,8 +19,28 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
         builder.Configuration.GetConnectionString("DefaultConnection")
     )
 );
+builder.Host.UseSerilog((ctx, lc) => lc
+    .ReadFrom.Configuration(ctx.Configuration)
+    .WriteTo.PostgreSQL(connectionString: ctx.Configuration.GetConnectionString("DefaultConnection"),
+        tableName: "LogEvents",
+        columnOptions: new Dictionary<string, ColumnWriterBase>
+        {
+            { "message", new RenderedMessageColumnWriter(NpgsqlDbType.Text) },
+            { "message_template", new MessageTemplateColumnWriter(NpgsqlDbType.Text) },
+            { "level", new LevelColumnWriter(true, NpgsqlDbType.Varchar) },
+            { "raise_date", new TimestampColumnWriter(NpgsqlDbType.TimestampTz) },
+            { "exception", new ExceptionColumnWriter(NpgsqlDbType.Text) },
+            { "properties", new LogEventSerializedColumnWriter(NpgsqlDbType.Jsonb) },
+            { "props_test", new PropertiesColumnWriter(NpgsqlDbType.Jsonb) },
+            { "machine_name", new SinglePropertyColumnWriter("MachineName", PropertyWriteMethod.ToString, NpgsqlDbType.Text, "l") }
+        },
+        needAutoCreateTable: true,
+        restrictedToMinimumLevel: LogEventLevel.Information
+    )
+    .WriteTo.Console()
+);
 var app = builder.Build();
-
+app.UseSerilogRequestLogging();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
